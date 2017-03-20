bb8bb584fd1bbf895c027ab0142cee426ce4817e
Jacob Salzberg <jsalzbergedu@yahoo.com>
2017-03-20 11:38:47 -0400

    Forgot to keep snapshots of my working through the tutorials here. Oh well, from now on I will

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+
+fn add_two(x: i32) -> i32{
+    x + 2
+}
+
+fn add_one(x: i32) -> i32{
+    x + 1
+}
 fn main () {
-    println!("Hello World!");
+    println!("hello world!");
+    let mut y: i32 = 1;
+    let mut f: fn(i32) -> i32 = add_one;
+    let y = f(y);
+    println!("y is equal to {}",y);
+    let f = add_two;
+    let y = f(y);
+    println!("y is equal to {}",y);
 }
