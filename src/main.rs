b9d53fa289336772321bad7c3d55cade00d0600f
Jacob Salzberg <jsalzbergedu@yahoo.com>
2017-03-20 13:26:47 -0400

    Just writing these to help me remember. Will probably only keep this code as one snapshot in the repo

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-
+   
 fn add_two(x: i32) -> i32{
     x + 2
 }
@@ -16,3 +16,17 @@ fn main () {
     let y = f(y);
     println!("y is equal to {}",y);
 }
+/* Numeric types:
+i8 -- i stands for integer (signed)
+i16
+i32
+i64 -- probably not supported on many systems
+u8 -- u stands for unsigned integer
+u16
+u32
+u64 -- probably not supported on many systems
+isize -- pointer sized integer
+usize
+f32 -- 32 bit floating
+f34 
+*/
