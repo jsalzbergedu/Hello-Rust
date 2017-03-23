3a17258c550ee62f77b9c079d8176aef7d9df957
Jacob Salzberg <jsalzbergedu@yahoo.com>
2017-03-23 16:05:14 -0400

    Slowly working through 'The Book' Carthago delenda est

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+/*
+use std::fmt::Debug;
+
+struct Rectangle<T> {
+    x: T,
+    y: T,
+    width: T,
+    height: T,
+}
+
+impl<T: PartialEq> Rectangle<T> {
+    fn is_square(&self) -> bool {
+        self.width == self.height
+    }
+}
+
+fn foo<T: Clone, K: Clone + Debug>(x: T, y: K) {
+    x.clone();
+    y.clone();
+    println!("{:?}", y);
+}
+
+fn bar<T, K>(x: T, y: K) where T: Clone, K: Clone + Debug {
+    x.clone();
+    y.clone();
+    println!("{:?}", y);
+}
+
+// Where clauses also allow bounds on left hand, e.g., T in the where T: Clone, to be types, e.g., i32
+
+fn main () {
+    let mut my_rect = Rectangle { x: 0, y: 0, width: 1, height: 1 };
+    if my_rect.is_square() {
+        println!("Very Good!");
+    }
+    my_rect.height = 0;
+    if !my_rect.is_square() {
+        println!("Genial!")
+    }
+    foo ("Hello", "World");
+    bar ("Bonjour", "Le Monde");
+}
+
+// Rules for impl traits
+//     1) If you are using a trait, i.e., from the standard library, you have to 'use' it first
+//        For instance: use std::io::Write to use the Write trait
+//     2) Trait/Type must be defined in the same crate that you are crate you write the impl in
+
+ */
+
+struct DootDoot;
+
+// Default method when you know how the other will be implemented:
+trait Foo {
+    fn is_valid(&self) -> bool;
+    fn is_invalid(&self) -> bool { !self.is_valid() };
+}
+
+// Overrides:
+struct DootDootFooOverridden;
