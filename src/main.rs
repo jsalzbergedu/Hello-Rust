26234eaf6b7cf178a71f6196c691fff59862e15a
Jacob Salzberg <jsalzbergedu@yahoo.com>
2017-03-24 13:33:31 -0400

    Working through 'The Book' got to functional-esqe prog

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ fn call_with_closure<F>(some_closure: F) -> i32
     some_closure(1)
 }
 
-
+fn pointer_add_one(i: i32) -> i32 {
+    i + 1
+}
+/*
 fn main() {
     let plus_one = |x: i32| x + 1; // | are pipes, through which the arguments of the closure go
     if plus_one(1) == 2 {
@@ -40,4 +43,25 @@ fn main() {
 
     let answer = call_with_closure(|x: i32| x + 23);
     println!("This answer is the result of a function that takes a closure {}", answer);
+    let function_pointer_demo = &pointer_add_one;
+    println!("This answer is the result of a function that takes a closure taking a function pointer {}", call_with_closure(&function_pointer_demo));
+}
+ */
+
+// Returning closures
+
+// This won't work:
+
+fn factory() -> Box<Fn(i32) -> i32> { // take a reference, "as referenes have a known size"
+    let num: i32 = 5;
+
+    Box::new(move |x: i32| x + num) //Almost works, exept closures borrow thier environment, i.e., num
+} // Move allows it to create a new stack frame so its not stuck in the one that becomes junk data
+
+fn main() {
+    let my_func_ptr = factory();
+    let answer = my_func_ptr(1);
+    if answer == 6 {
+        println!("Very good!");
+    }
 }
