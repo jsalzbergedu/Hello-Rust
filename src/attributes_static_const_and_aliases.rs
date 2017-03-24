cbdf31b7dbd9134e46e8dde7418b8243861d91e6
Jacob Salzberg <jsalzbergedu@yahoo.com>
2017-03-24 16:38:11 -0400

    Working through the book. Slow and steady wins the race!

---
+++ src/attributes_static_const_and_aliases.rs
@@ -0,0 +1,40 @@
+// Skipping (gasp) crates because I already have to use them elsewhere
+
+// const and static: this should be quick
+
+static mut JE_SUIS_MONDIAL: i32 = 10;
+
+/*
+fn main() {
+    const ABACKDEE: i32 = 4;
+    println!("I am inlined wherever I'm found: {}", ABACKDEE);
+    unsafe {
+        JE_SUIS_MONDIAL += JE_SUIS_MONDIAL;
+        println!("Modifying static mut s is not safe (bad): {}", JE_SUIS_MONDIAL);
+    }
+}
+ */
+
+// Since this is such a small file, I'll also include attributes
+
+// Declared for functions like this:
+#[test]
+fn test_func() {
+    println!("I am a test function only compiled with --test.");
+}
+
+// #[main] exept for now this cannot be used
+fn not_main () {
+    println!("I become the entry point");
+}
+
+fn main() {
+    println!("Another one of interest is should_panic, which inverts the sucess condition.");
+    // These are some really short chapters, so I'll play with aliases here too.
+    type number = i32;
+    let x: number = 5;
+    let y: number = 5;
+    if x == y {
+        println!("Aliases are just cut&paste for types");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,23 @@
-// Skipping (gasp) crates because I already have to use them elsewhere
+// Coercion:
+// let, as, const, and static
+// is implicit, e.g., &mut T is passed as &T to a function
 
-// const and static: this should be quick
+use std::mem;
 
-static mut JE_SUIS_MONDIAL: i32 = 10;
-
-/*
 fn main() {
-    const ABACKDEE: i32 = 4;
-    println!("I am inlined wherever I'm found: {}", ABACKDEE);
+    // safe casts:
+    // Numeric casts
+    let x: i32 = 7;
+    let y = x as i64;
+
+    // Pointer casts
+    let a = 343 as *const char;
+    let b = a as u32;
+    // The above operation is safe
+    // The following however is not safe
     unsafe {
-        JE_SUIS_MONDIAL += JE_SUIS_MONDIAL;
-        println!("Modifying static mut s is not safe (bad): {}", JE_SUIS_MONDIAL);
-    }
-}
- */
-
-// Since this is such a small file, I'll also include attributes
-
-// Declared for functions like this:
-#[test]
-fn test_func() {
-    println!("I am a test function only compiled with --test.");
-}
-
-// #[main] exept for now this cannot be used
-fn not_main () {
-    println!("I become the entry point");
-}
-
-fn main() {
-    println!("Another one of interest is should_panic, which inverts the sucess condition.");
-    // These are some really short chapters, so I'll play with aliases here too.
-    type number = i32;
-    let x: number = 5;
-    let y: number = 5;
-    if x == y {
-        println!("Aliases are just cut&paste for types");
+        let c = [0u8, 1u8, 0u8, 0u8];
+        let d: u32 = mem::transmute(c);
+        println!("This is not gaurenteed to be safe, {:?}", c);
     }
 }
